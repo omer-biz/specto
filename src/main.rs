e137092ebf1e1bf9543ef539462ac1b3eea5ae27
omer <omerabdi@protonmail.com>
2024-01-11 21:16:12 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use std::{fs::File, path::PathBuf, process::Command};
+
+use clap::Parser;
+use tiny_http::{Response, Server};
+
+#[derive(Parser, Debug)]
+pub struct Args {
+    /// elm source file to watch
+    #[arg(default_value = "src/Main.elm", value_name = "<elm-source>")]
+    source: PathBuf,
+
+    /// Address to bind to.
+    #[arg(short, long, value_name = "ip:port", default_value = "localhost:9000")]
+    address: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    // 1. check if the file exists
+    // 2. compile it to index.html
+    // 3. inject index.html with some websocket stuff
+    // 4. serve index.html
+    // 5. on another thread interact interact with the websocket client
+    // 6. watch the elm file if ther is any change go to 2
+
+    if !args.source.exists() {
+        panic!("File: {:?} not found.", args.source);
+    }
+
+    let mut elm_make = Command::new("elm");
+    elm_make.arg("make").arg(&args.source);
+    let _output = elm_make.output().expect("Failed to execute a process");
+
+    let server = Server::http(args.address).expect("Failed to bind to address");
+    for request in server.incoming_requests() {
+        println!("url: {}", request.url());
+        let elm_source = File::open("index.html").expect("Failed to open file");
+        let response = Response::from_file(elm_source);
+
+        request.respond(response).expect("Failed to send response");
+    }
+
+    Ok(())
+}
